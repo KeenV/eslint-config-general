91c4efc1444885fdc779398dd4e9e44f02529f62
keen.v <keen.w@outlook.com>
2021-10-05 00:09:02 +0800

    feat: add ES6 rules

---
+++ rules/es6.js
@@ -0,0 +1,83 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  rules: {
+    'arrow-body-style': ['warn', 'as-needed', {
+      requireReturnForObjectLiteral: false,
+    }],
+    'arrow-parens': ['warn', 'as-needed', {
+      requireForBlockBody: true,
+    }],
+    'arrow-spacing': ['warn', { before: true, after: true }],
+    'constructor-super': 'error',
+    'generator-star-spacing': ['error', { before: false, after: true }],
+    'no-class-assign': 'error',
+    'no-confusing-arrow': ['error', {
+      allowParens: true,
+    }],
+    'no-const-assign': 'error',
+    'no-dupe-class-members': 'error',
+    'no-duplicate-imports': ['error', { includeExports: true }],
+    'no-new-symbol': 'error',
+    'no-restricted-exports': ['off', {
+      restrictedNamedExports: [
+        'default', // use `export default` to provide a default export
+        'then', // this will cause tons of confusion when your module is dynamically `import()`ed
+      ],
+    }],
+    'no-restricted-imports': ['off', {
+      paths: [],
+      patterns: [],
+    }],
+    'no-this-before-super': 'error',
+    'no-useless-computed-key': 'error',
+    'no-useless-constructor': 'error',
+    'no-useless-rename': ['error', {
+      ignoreDestructuring: false,
+      ignoreImport: false,
+      ignoreExport: false,
+    }],
+    'no-var': 'error',
+    'object-shorthand': ['error', 'always', {
+      ignoreConstructors: false,
+      avoidQuotes: true,
+    }],
+    'prefer-arrow-callback': ['error', {
+      allowNamedFunctions: false,
+      allowUnboundThis: true,
+    }],
+    'prefer-const': ['error', {
+      destructuring: 'any',
+      ignoreReadBeforeAssign: true,
+    }],
+    'prefer-destructuring': ['warn', {
+      VariableDeclarator: {
+        array: false,
+        object: true,
+      },
+      AssignmentExpression: {
+        array: true,
+        object: false,
+      },
+    }, {
+      enforceForRenamedProperties: false,
+    }],
+    'prefer-numeric-literals': 'error',
+    'prefer-reflect': 'off',
+    'prefer-rest-params': 'off',
+    'prefer-spread': 'off',
+    'prefer-template': 'off',
+    'require-yield': 'error',
+    'rest-spread-spacing': ['error', 'never'],
+    'sort-imports': ['off', {
+      ignoreCase: false,
+      ignoreDeclarationSort: false,
+      ignoreMemberSort: false,
+      memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+    }],
+    'symbol-description': 'error',
+    'template-curly-spacing': ['error', 'never'],
+    'yield-star-spacing': ['error', 'after'],
+  },
+}
